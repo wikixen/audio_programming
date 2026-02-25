6cd2d61f87d757f9d8b592064744fc7ea20091cf
wikixen <bennylopezhernandez0@gmail.com>
2026-02-24 21:29:31 -0500

    Update midi2freq with user input & freq2midi with correct logic

--- chapter1/src/freq2midi.c
+++ chapter1/src/freq2midi.c
@@ -12,9 +12,10 @@ int main()
    * c0 = frequency of MIDI Note 0 ie lowest possible note
    * c5 = frequency of Middle C ie MIDI note 60
    */
-  double semitone_ratio = pow(2, 1.0 / 12.0),
-         c5 = 220.0 * pow(semitone_ratio, 3.0),
-         c0 = c5 * pow(0.5, 5.0);
+  double semitone_ratio, c5, c0;
+  semitone_ratio = pow(2, 1.0 / 12.0),
+  c5 = 220.0 * pow(semitone_ratio, 3.0),
+  c0 = c5 * pow(0.5, 5.0);
 
   double frequency = 430.0;
 
@@ -24,14 +25,24 @@ int main()
   printf("The nearest MIDI note to the frequency %.2f is %d\n", frequency, midinote);
   
   // Exercise 1.2.6 PG.77
-  int midiL = floor(fracmidi),
-      midiH = ceil(fracmidi);
+  int midiL, midiH;
+  midiL = floor(fracmidi),
+  midiH = ceil(fracmidi);
 
   double freqL = c0 * pow(semitone_ratio, midiL);
   double freqH = c0 * pow(semitone_ratio, midiH);
 
-  int bend = (int)(((freqH - frequency) / (freqH - freqL)) * 100 * -1);
-
+  int bend = 0;
+  // If the frequency is closer to the higher frequency the frequency-(higher frequency), else frequency-(lower frequenct)
+  if (midiH == fracmidi)
+  {
+    bend = (int)(((freqH - frequency) / (freqH - freqL)) * 100);
+  }
+  else
+  {
+    bend = (int)(((freqL - frequency) / (freqH - freqL)) * 100);
+  }
+  
   printf("pitchbend = %d%%\n", bend);
 
   return 0;
--- chapter1/src/midi2freq.c
+++ chapter1/src/midi2freq.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 /**
@@ -12,19 +13,41 @@ int main()
    * c0 = frequency of MIDI Note 0 ie lowest possible note
    * c5 = frequency of Middle C ie MIDI note 60
    */
-  double semitone_ratio = pow(2, 1.0 / 12.0),
-         c5 = 220.0 * pow(semitone_ratio, 3.0),
-         c0 = c5 * pow(0.5, 5.0);
+  double semitone_ratio, c5, c0;
+  semitone_ratio = pow(2, 1.0 / 12.0),
+  c5 = 220.0 * pow(semitone_ratio, 3.0),
+  c0 = c5 * pow(0.5, 5.0);
 
-  // Actual calculation of frequency of provided note
-  int midinote = 72; //C# above A=440
+  printf("Enter a MIDI note(0-127): ");
+  char message[256];
+  if (gets(message)==NULL)
+  {
+    printf("ERROR: There was an error reading your input.\n");
+    return 1;
+  }
+
+  if (message[0]=='\0')
+  {
+    printf("Have a nice day!\n");
+    return 1;
+  }
+  
+  int midinote = atoi(message);
+  if (midinote<0 || midinote>127)
+  {
+    printf("ERROR: %s isn't a valid MIDI note. Make sure your note is in the range 0-127.\n");
+    return 1;
+  }
+  
+  // Calculation of frequency of provided note
   double frequency = c0 * pow(semitone_ratio, midinote);
 
   // Exercise 1.2.5 PG.77
-  // Only works for MIDI note 60-72 ie one octave 
-  double c0_length = 660,
-         c5_length = 330,
-         length_interval = (c0_length - c5_length) / 12;
+  // Only works for MIDI note 60-72 ie one octave
+  double c0_length, c5_length, length_interval;
+  c0_length = 660,
+  c5_length = 330,
+  length_interval = (c0_length - c5_length) / 12;
 
   double midinote_length = 330+((midinote%12) * length_interval);
 
