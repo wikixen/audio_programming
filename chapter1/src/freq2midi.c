42d69437d03509d4e676097a4d4d1056fa6b28f0
wikixen <bennylopezhernandez0@gmail.com>
2026-02-25 16:33:25 -0500

    Updated midi2freq with more streamlined logic as well as ability to use custome root A

--- chapter1/src/freq2midi.c
+++ chapter1/src/freq2midi.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 /**
  * This program finds the MIDI note number associated with the given frequency
@@ -15,26 +16,23 @@ int main()
    */
   double semitone_ratio, c5, c0;
   semitone_ratio = pow(2, 1.0 / 12.0);
-  c5 = 220.0 * pow(semitone_ratio, 3.0);
+  // c5 = 220.0 * pow(semitone_ratio, 3.0);
+  c5 = 207.5 * pow(semitone_ratio, 3.0);
   c0 = c5 * pow(0.5, 5.0);
 
-  printf("Enter a MIDI note(0-127): ");
+  printf("Enter a frequency that isn't negative: ");
   char message[256];
   if (fgets(message, sizeof(message), stdin) == NULL)
-  {
-    /* code */
-  }
-  
-  if (gets(message)==NULL)
   {
     printf("ERROR: There was an error reading your input.\n");
     return 1;
   }
+  message[strcspn(message, "\n")] = '\0';
 
   double frequency = atof(message);
   if (frequency < 0)
   {
-    printf("ERROR: %s isn't a valid frequency. Make sure your frequency is 0 or greater.\n");
+    printf("ERROR: %s isn't a valid frequency. Make sure your frequency is 0 or greater.\n", message);
     return 1;
   }
 
--- chapter1/src/midi2freq.c
+++ chapter1/src/midi2freq.c
@@ -7,53 +7,44 @@
  * This program calculates the frequency of a MIDI note number
  */
 
-int main()
+int main(int argc, char *argv[])
 {
-  /**
-   * semitone_ratio = ~1.0594631; Is the amount of change in frequency between a semitone
-   * c0 = frequency of MIDI Note 0 ie lowest possible note
-   * c5 = frequency of Middle C ie MIDI note 60
-   */
-  double semitone_ratio, c5, c0;
-  semitone_ratio = pow(2, 1.0 / 12.0);
-  c5 = 220.0 * pow(semitone_ratio, 3.0);
-  c0 = c5 * pow(0.5, 5.0);
-
-  printf("Enter a MIDI note(0-127): ");
-  char message[256];
-  if (fgets(message, sizeof(message), stdin) == NULL)
+  // Usage description
+  if (argc != 3)
   {
-    printf("ERROR: There was an error reading your input.\n");
+    printf("%s : converts MIDI note to frequency.\n", argv[0]);
+    printf("  usage: %s rootAFrequency MIDInote\n", argv[0]);
+    printf("  range: 0 <= rootAFrequency\n");
+    printf("  range: 0 <= MIDInote <= 127\n");
+
     return 1;
   }
-  message[strcspn(message, "\n")] = '\0'; // Strips '\n' from message & replaces it with '\0'
 
-  if (message[0]=='\0')
+  // a4 is the root A frequency in Hz
+  double a4 = strtof(argv[1], NULL);
+  if (a4 < 0)
   {
-    printf("Have a nice day!\n");
-    return 0;
+    printf("ERROR: %s frequency isn't a valid root frequency. Enter a value 0 or greater.\n", argv[1]);
   }
-  
-  int midinote = atoi(message);
-  if (midinote<0 || midinote>127)
+
+  int midinote = strtol(argv[2], NULL, 10);
+  if (midinote < 0 || midinote > 127)
   {
-    printf("ERROR: %s isn't a valid MIDI note. Make sure your note is in the range 0-127.\n", message);
+    printf("ERROR: %s isn't a valid MIDI note. Make sure your note is in the range 0-127.\n", argv[2]);
     return 1;
   }
-  
-  // Calculation of frequency of provided note
-  double frequency = c0 * pow(semitone_ratio, midinote);
+
+  // Calculation of frequency for provided MIDI note
+  double f = a4 * pow(2, (midinote - 69.0) / 12.0);
 
   // Exercise 1.2.5 PG.77
-  // Only works for MIDI note 60-72 ie one octave
-  double c0_length, c5_length, length_interval;
-  c0_length = 660;
-  c5_length = 330;
-  length_interval = (c0_length - c5_length) / 12;
+  // // Only works for MIDI note 60-72 ie one octave
+  // double c0_length, c5_length, length_interval;
+  // c0_length = 660; // Length of MIDI note 0
+  // c5_length = 330; // Length og MIDI note for middle C, 60 in this case
+  // length_interval = (c0_length - c5_length) / 12;
 
-  double midinote_length = 330+((midinote%12) * length_interval);
+  // double midinote_length = 330+((midinote%12) * length_interval);
 
-  printf("MIDI Note %d has frequency %f & a string length of %.2f\n", midinote, frequency, midinote_length);
-
-  return 0;
+  printf("MIDI note: %d has a frequency of %.2f\n", midinote, f);
 }
